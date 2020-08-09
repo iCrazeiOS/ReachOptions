b41643c833061f1801eb69e80ccd007d9ed6da07
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2020-08-09 22:49:30 +0100

    added newline to control
    removed ugly whitespace in iPod/iPad checker
    
    moved control to the root folder

--- Tweak/UIDevice+notchedDevice.m
+++ Tweak/UIDevice+notchedDevice.m
@@ -1,16 +1,16 @@
 #include "UIDevice+notchedDevice.h"
 #import <LocalAuthentication/LocalAuthentication.h>
 
-@implementation UIDevice (notchedDevice) 
+@implementation UIDevice (notchedDevice)
 
 -(BOOL)isNotched {
-    
+
     if([self isAnIpod] || [self isAnIpad]) { // iPad and iPod are not notched devices
         return NO;
     }
-    
+
     LAContext *context = [[LAContext alloc] init];
-    
+
     [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil];
     return context.biometryType == LABiometryTypeFaceID; // only devices with FaceID are notched atm (also the latest iPad PRO so that's why i added an iPad check)
 }
@@ -21,8 +21,7 @@
 
 -(BOOL)isAnIpod {
     NSString const *model = [UIDevice.currentDevice model]; // get the device model
-    
     return ([model isEqualToString:@"iPod"]);
-} 
+}
 
 @end
