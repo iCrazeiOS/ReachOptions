7f00a920cca4d27dd860bffa1f195fd90f57cd2b
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2020-08-09 22:54:18 +0100

    not needed anymore

--- Preferences/ROPRootListController.h
+++ Preferences/ROPRootListController.h
@@ -1,4 +1,4 @@
- 
+
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 #import <CepheiPrefs/HBRootListController.h>
@@ -6,5 +6,4 @@
 #import <Cephei/HBPreferences.h>
 
 @interface ROPRootListController : HBListController
-@property(nonatomic, retain) UIBarButtonItem *respringApplyButton;
 @end
